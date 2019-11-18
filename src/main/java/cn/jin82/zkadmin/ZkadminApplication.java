c61364216d4990b6f0f322492c119153661614ab
jin82 <jin820100449@qq.com>
2019-11-18 17:35:04 +0800

    Initial commit

---
+++ src/main/java/cn/jin82/zkadmin/ZkadminApplication.java
@@ -0,0 +1,13 @@
+package cn.jin82.zkadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/jin82/zkadmin/ZkadminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jin82.zkadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
